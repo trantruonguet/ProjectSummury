83dfe46a5a244976d656add6d7786501098a1a5d
Truongtv <truongtv@rikkeisoft.com>
2020-02-03 16:14:28 +0700

    Initial Commit

---
+++ ProjectSummury/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSummury
+//
+//  Created by TruongTV-GD on 2/3/20.
+//  Copyright © 2020 TruongTV-GD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
