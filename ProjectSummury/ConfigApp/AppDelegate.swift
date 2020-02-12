389c6107bbf79dc5917a2993b3fa0f8d3934dacd
Minh Nguyen <minhotmt@gmail.com>
2020-02-12 16:38:30 +0700

    add lazy loding demo

--- ProjectSummury/ConfigApp/AppDelegate.swift
+++ ProjectSummury/ConfigApp/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        let home = HomeVC()
+        window?.rootViewController = home
         return true
     }
 
